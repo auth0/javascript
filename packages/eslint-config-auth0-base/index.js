412fbbe1fc48ed008c057d8995b4ea26e9768c69
Ramiro Silveyra d'Avila <ramasilveyra1@gmail.com>
2017-01-28 20:15:52 -0300

    Add `_id` exception to `no-underscore-dangle` rule

--- packages/eslint-config-auth0-base/index.js
+++ packages/eslint-config-auth0-base/index.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: [
     'eslint-config-airbnb-base',
     './rules/errors',
+    './rules/style',
     './rules/variables'
   ].map(require.resolve),
   parserOptions: {
